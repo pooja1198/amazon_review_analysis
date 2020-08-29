c27ca2e4ce98ef9c09e9625734843c1c51500afb
Pooja Patwari <poojapatwari4@gmail.com>
2020-08-29 20:52:08 +0530

    Initializing Repo

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Proejct for AI Champ Task',
+    author='Pooja Patwari',
+    license='MIT',
+)
